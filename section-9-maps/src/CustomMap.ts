7ab877322b9b6cb3e4ca1346ed7696316f7cb883
Gregor Zurowski <gregor@zurowski.net>
2020-06-20 19:14:57 +0200

    Add section 9 sample
    Add sample for section 9 "Design Patterns with TypeScript"

---
+++ section-9-maps/src/CustomMap.ts
@@ -0,0 +1,41 @@
+// Section 9
+export interface Mappable {
+  location: {
+    lng: number;
+    lat: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+      zoom: 1,
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
---
+++ section-9-maps/src/User.ts
@@ -0,0 +1,24 @@
+// Section 9
+import faker from 'faker';
+import { Mappable } from './CustomMap';
+
+export class User implements Mappable {
+  name: string;
+  color: string = 'red';
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `User Name: ${this.name}`;
+  }
+}
