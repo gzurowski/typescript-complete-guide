31e7f751384e9ce5a20626b166d80628fbdd7a3a
Gregor Zurowski <gregor@zurowski.net>
2020-06-21 22:13:39 +0200

    Update section 13 sources
    Composition based approach as of chapter 173

--- section-13-web/src/index.ts
+++ section-13-web/src/index.ts
@@ -1,4 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Elaine', age: 40 });
+const user = new User({ id: 1, name: 'Kramer', age: 30 });
+
+user.on('save', () => {
+  console.log(user);
+});
+
 user.save();
