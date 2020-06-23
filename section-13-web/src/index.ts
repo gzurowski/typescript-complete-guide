3f32855f1bcb73bfd5337a3f4e59b067b47ab911
Gregor Zurowski <gregor@zurowski.net>
2020-06-23 09:23:26 +0200

    Update section 13 sample as of chapter 196

--- section-13-web/src/index.ts
+++ section-13-web/src/index.ts
@@ -1,9 +1,12 @@
-import { User, UserProps } from './models/User';
+import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const collection = User.buildUserCollection();
+const user = User.buildUser({ name: 'NAME', age: 30 });
 
-collection.on('change', () => {
-  console.log(collection);
-});
-
-collection.fetch();
+const root = document.getElementById('root');
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('Root element not found');
+}
